bd8327934182960e82af8cd00bbd1d4bb9630f0c
rikako <jc180215b@gmail.com>
2023-03-02 23:01:29 +0900

    modify:定数、変数、名前のルール

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-console.log('hello');
+//変数
+//countに0を代入する
+//
+let count;
+//undefined
+console.log(count);
+count = 30;
+console.log(count);
+
+//定数(1箇所しか変えなくていい)
+//できる限りconstを使う
+//constは再代入できない！
+//constは初期値を必ず定義する
+const daysInWeek = 7;
+
+//名前のルール
+//$ _ は入れることができる
+//数字は先頭に来てはいけない
+//既に予約されたlet,constなど使えない
+//CamelCase(一番いい方法)
+//snake_case
