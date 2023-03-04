24ae664a1e9a331ab5185aaf954c2960d4e65228
rikako <jc180215b@gmail.com>
2023-03-04 11:56:00 +0900

    modify:データの型

--- main.js
+++ main.js
@@ -33,3 +33,58 @@ result += 10;
 //ややこしくなる
 result++;
 ++result;
+
+//型: type
+//数値型
+//Integer
+let number = 10;
+number = -4;
+
+//string
+const userName = 'Rikako';
+let string = 'Hello';
+string = 'Hello' + userName;
+string = `Hello ${userName}`;
+console.log(string);
+
+//特殊な文字はバックスラッシュを使う
+//バッククオートを使うと改行できる・変数が扱える
+string = `Hello
+Rikako`;
+console.log(string);
+
+//動的型付け
+//状況によって変化する
+
+//静的
+//一定の値
+
+//どうやって型変換ができているのか？
+//明示的に型変換する方法
+const userInput = '10.9';
+//20.9
+calcResult = Number(userInput) + 10;
+console.log(calcResult);
+
+//20
+calcResult2 = parseInt(userInput) + 10;
+console.log(calcResult2);
+
+//20.9
+calcResult3 = parseFloat(userInput) + 10;
+console.log(calcResult3);
+
+//20.9
+calcResult4 = +userInput + 10;
+console.log(calcResult4);
+
+//文字列にするには自動に変換される
+
+//真偽値
+let boolean = true;
+boolean = false;
+
+//配列を使って値を順序つけてまとめる方法
+let array = ['apple', 'banana', 'grape'];
+array.push('peach');
+console.log(array);
