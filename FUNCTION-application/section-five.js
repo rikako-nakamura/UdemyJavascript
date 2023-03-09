3642e1e546f476c7bafd6f0311349551d71990d9
rikako <jc180215b@gmail.com>
2023-03-09 22:48:53 +0900

    modify:関数式について

--- FUNCTION-application/section-five.js
+++ FUNCTION-application/section-five.js
@@ -1,3 +1,4 @@
+// 関数宣言文
 function add(a, b) {
   return a + b;
 }
@@ -6,3 +7,10 @@ console.log(add);
 console.dir(add);
 const newAdd = add;
 console.log(newAdd(3, 4));
+
+// 関数式
+// オブジェクトのみを作ってる
+let sayHi = function () {};
+console.log(sayHi);
+
+// どっちがいいのか？
