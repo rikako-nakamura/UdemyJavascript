52e2c418f851abd424469ec905dc453a030c5388
rikako <jc180215b@gmail.com>
2023-03-13 22:07:27 +0900

    modify:varについて

--- INTERNAL-Javascript/section-six.js
+++ INTERNAL-Javascript/section-six.js
@@ -1,3 +1,17 @@
 // グローバルオブジェクト
 // プロパティが一覧で見れる
 console.log(globalThis);
+
+// var:変数の定義
+// 再宣言できる (最後が優先される)
+var hello = 'hello';
+var hello = 'hi';
+
+// 関数の中でしかアクセスできない
+function sayTomato() {
+  var tomato = 'tomato';
+  console.log(tomato);
+}
+
+// varで宣言したものはglobalThisに追加できる
+// 初期値は巻き上げない
